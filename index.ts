d1b1dd5d1f4c53cd9f7b871176b4108d14fd40e2
Alan Casas <alancasasarevalo@gmail.com>
2019-06-02 15:08:54 +0200

    no message

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-import Server from "./server/server";
+import Server from "./src/server/server";
+import router from "./src/router/router";
 
 const server = Server.init(3000);
+
+server.app.use(router);
 
 server.start( () => {
     console.log('servidor corriendo en el puerto 3000')
