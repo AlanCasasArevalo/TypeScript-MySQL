a37dccbee2cb75a81baf0f6df5e6af8cb815819f
Alan Casas <alancasasarevalo@gmail.com>
2019-06-02 10:56:30 +0200

    Server TypeScript was init

---
+++ src/server/server.ts
@@ -0,0 +1,46 @@
+import express = require('express');
+import * as path from "path";
+
+export default class Server {
+    public app!: express.Application;
+    public port: number;
+
+    constructor (port: number) {
+        this.port = port;
+        this.app = express();
+    }
+
+    static init (port: number){
+        return new Server(port);
+    }
+
+    private publicFolder () {
+        const publicPath = path.resolve(__dirname, '../public')
+        this.app.use(express.static(publicPath));
+    }
+
+    start( callback: () => void ){
+        this.app.listen( this.port, callback )
+        this.publicFolder()
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
